31bddffbd9b7a3fb2c9fbf2e622b3fe3bf7f1820
Sanghyuk Park <sanghyuk.park85@gmail.com>
2020-06-18 14:23:49 -0500

    Change the basic colors of the theme to mimic the Solarized-light

--- themes/doom-one-light-theme.el
+++ themes/doom-one-light-theme.el
@@ -33,7 +33,7 @@ determine the exact padding."
   "A light theme inspired by Atom One"
 
   ;; name        default   256       16
-  ((bg         '("#fafafa" nil       nil            ))
+  ((bg         '("#002b36" nil       nil            ))
    (bg-alt     '("#f0f0f0" nil       nil            ))
    (base0      '("#f0f0f0" "#f0f0f0" "white"        ))
    (base1      '("#e7e7e7" "#e7e7e7" "brightblack"  ))
@@ -44,21 +44,21 @@ determine the exact padding."
    (base6      '("#202328" "#2e2e2e" "brightblack"  ))
    (base7      '("#1c1f24" "#1e1e1e" "brightblack"  ))
    (base8      '("#1b2229" "black"   "black"        ))
-   (fg         '("#383a42" "#424242" "black"        ))
+   (fg         '("#819195" "#424242" "black"        ))
    (fg-alt     '("#c6c7c7" "#c7c7c7" "brightblack"  ))
 
    (grey       base4)
-   (red        '("#e45649" "#e45649" "red"          ))
-   (orange     '("#da8548" "#dd8844" "brightred"    ))
-   (green      '("#50a14f" "#50a14f" "green"        ))
+   (red        '("#dc322f" "#e45649" "red"          ))
+   (orange     '("#cb4b16" "#dd8844" "brightred"    ))
+   (green      '("#859900" "#50a14f" "green"        ))
    (teal       '("#4db5bd" "#44b9b1" "brightgreen"  ))
-   (yellow     '("#986801" "#986801" "yellow"       ))
-   (blue       '("#4078f2" "#4078f2" "brightblue"   ))
-   (dark-blue  '("#a0bcf8" "#a0bcf8" "blue"         ))
-   (magenta    '("#a626a4" "#a626a4" "magenta"      ))
-   (violet     '("#b751b6" "#b751b6" "brightmagenta"))
-   (cyan       '("#0184bc" "#0184bc" "brightcyan"   ))
-   (dark-cyan  '("#005478" "#005478" "cyan"         ))
+   (yellow     '("#b58900" "#986801" "yellow"       ))
+   (blue       '("#268bd2" "#4078f2" "brightblue"   ))
+   (dark-blue  '("#043c52" "#a0bcf8" "blue"         ))
+   (magenta    '("#d33682" "#a626a4" "magenta"      ))
+   (violet     '("#6c71c4" "#b751b6" "brightmagenta"))
+   (cyan       '("#2aa198" "#0184bc" "brightcyan"   ))
+   (dark-cyan  '("#054048" "#005478" "cyan"         ))
 
    ;; face categories -- required for all themes
    (highlight      blue)
