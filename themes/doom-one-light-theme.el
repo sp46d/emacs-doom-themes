0f33f5d7081374728e812418ad2c5817ddd56938
Sanghyuk Park <sanghyuk.park85@gmail.com>
2020-06-18 20:58:23 -0500

    Change the foreground color for the 256 mode

--- themes/doom-one-light-theme.el
+++ themes/doom-one-light-theme.el
@@ -44,7 +44,7 @@ determine the exact padding."
    (base6      '("#202328" "#2e2e2e" "brightblack"  ))
    (base7      '("#1c1f24" "#1e1e1e" "brightblack"  ))
    (base8      '("#1b2229" "black"   "black"        ))
-   (fg         '("#002b36" "#424242" "black"        ))
+   (fg         '("#002b36" "#002b36" "black"        ))
    (fg-alt     '("#c6c7c7" "#c7c7c7" "brightblack"  ))
 
    (grey       base4)
