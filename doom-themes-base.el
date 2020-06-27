2f69d5852cf8df7107f6fa68b22e1dd281a1b892
Sanghyuk Park <sanghyuk.park85@gmail.com>
2020-06-27 15:51:51 -0500

    Make a final change on Company tooltip

--- doom-themes-base.el
+++ doom-themes-base.el
@@ -366,11 +366,11 @@
     (cfw:face-select             :background region)
 
     ;; company
-    (company-tooltip            :foreground fg :background (doom-blend blue bg 0.1))
+    (company-tooltip            :foreground fg :background (doom-blend blue bg 0.05))
     (company-tooltip-common     :foreground magenta :distant-foreground base0 :weight 'bold)
     (company-tooltip-search     :background magenta :foreground bg :distant-foreground fg :weight 'bold)
     (company-tooltip-search-selection :foreground magenta :weight 'bold)
-    (company-tooltip-selection  :background (doom-blend blue bg 0.25))
+    (company-tooltip-selection  :background (doom-blend blue bg 0.20))
     (company-tooltip-mouse      :background blue :foreground bg :distant-foreground fg)
     (company-tooltip-annotation :foreground cyan :distant-foreground bg)
     (company-scrollbar-bg       :inherit 'tooltip)
